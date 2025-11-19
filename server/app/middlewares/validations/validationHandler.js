3f91f822d10a331a6d2daae8527f32b74f3f5a69
kimwemin <kimwemin488@gmail.com>
2025-11-19 17:38:57 +0900

    251119 validation, response body, type

---
+++ server/app/middlewares/validations/validationHandler.js
@@ -0,0 +1,21 @@
+/**
+ * @file app/middlewares/validations/validationHandler.js
+ * @description 유효성 검사 핸들러
+ * 251119 v1.0.0 김위민 init
+ */
+
+import { validationResult } from "express-validator";
+import { BAD_REQUEST_ERROR } from "../../../configs/responseCode.config.js";
+import { createBaseResponse } from "../../utils/createBaseResponse.util.js";
+
+export default function validationHandler(req, res, next) {
+  const errors = validationResult(req);
+
+  // 에러 발생 여부 확인
+  if(!errors.isEmpty()) {
+    // 에러 처리
+    return res.status(BAD_REQUEST_ERROR.status).send(createBaseResponse(BAD_REQUEST_ERROR, errors.array()));
+  }
+  // 에러가 일어나지 않았을 경우 다음 처리로 넘김
+  next();
+}
---
+++ server/routes/auth.router.js
@@ -0,0 +1,19 @@
+/**
+ * @file routes/auth.router.js
+ * @description 인증 관련 라우터
+ * 251119 v1.0.0 김위민 init
+ */
+
+import express from 'express';
+import { authController } from '../app/controllers/auth.controller.js';
+import loginValidator from '../app/middlewares/validations/validators/auth/login.validator.js';
+import validationHandler from '../app/middlewares/validations/validationHandler.js';
+
+const authRouter = express.Router();
+
+// client -> app.js -> router -> middleware(필요할 경우에만) -> controller -> 비지니스 로직(service)에서 연산 처리 -> repagitory -> service -> controller -> client
+// router는 컨트롤러로 연결하는 연결책에 불과하다
+// return은 후속 동작이 없을 때 확실히 작업을 끝내기 위해 사용
+authRouter.post('/login', loginValidator, validationHandler, authController.login);
+
+export default authRouter;
